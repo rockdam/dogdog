72ab91fdaf6b18d932149ac2d519e0fe0740051e
rockdam <choi37338@naver.com>
2020-09-15 21:19:39 +0900

    커스텀 달력 만드는 중

--- app/src/main/java/com/makeus/dogdog/src/HomeDogDog/TrackingNoteFragment/TrackingNote.java
+++ app/src/main/java/com/makeus/dogdog/src/HomeDogDog/TrackingNoteFragment/TrackingNote.java
@@ -1,5 +1,6 @@
 package com.makeus.dogdog.src.HomeDogDog.TrackingNoteFragment;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,8 +8,18 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.TextView;
 
 import com.makeus.dogdog.R;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,6 +28,20 @@ import com.makeus.dogdog.R;
  */
 public class TrackingNote extends Fragment {
 
+    GridView mCalendar;
+
+    /**
+     * 일 저장 할 리스트
+     */
+    private ArrayList<String> dayList;
+    private Calendar mCal;
+    /**
+     * 캘린더 변수
+     */
+    /**
+     * 그리드뷰 어댑터
+     */
+    private GridAdapter gridAdapter;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -55,12 +80,142 @@ public class TrackingNote extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+
     }
 
+    public void createCalendar()
+    {
+// 오늘에 날짜를 세팅 해준다.
+        long now = System.currentTimeMillis();
+        final Date date = new Date(now);
+        //연,월,일을 따로 저장
+        final SimpleDateFormat curYearFormat = new SimpleDateFormat("yyyy", Locale.KOREA);
+        final SimpleDateFormat curMonthFormat = new SimpleDateFormat("MM", Locale.KOREA);
+        final SimpleDateFormat curDayFormat = new SimpleDateFormat("dd", Locale.KOREA);
+
+        //현재 날짜 텍스트뷰에 뿌려줌
+        // tvDate.setText(curYearFormat.format(date) + "/" + curMonthFormat.format(date));
+
+        //gridview 요일 표시
+        dayList = new ArrayList<String>();
+        dayList.add("일");
+        dayList.add("월");
+        dayList.add("화");
+        dayList.add("수");
+        dayList.add("목");
+        dayList.add("금");
+        dayList.add("토");
+
+        mCal = Calendar.getInstance();
+
+        //이번달 1일 무슨요일인지 판단 mCal.set(Year,Month,Day)
+        mCal.set(Integer.parseInt(curYearFormat.format(date)), Integer.parseInt(curMonthFormat.format(date)) - 1, 1);
+        int dayNum = mCal.get(Calendar.DAY_OF_WEEK);
+        //1일 - 요일 매칭 시키기 위해 공백 add
+        for (int i = 1; i < dayNum; i++) {
+            dayList.add("");
+        }
+        setCalendarDate(mCal.get(Calendar.MONTH) + 1);
+
+        gridAdapter = new GridAdapter(getContext(), dayList);
+        mCalendar.setAdapter(gridAdapter);
+
+    }
+
+    /**
+     * 해당 월에 표시할 일 수 구함
+     *
+     * @param month
+     */
+    private void setCalendarDate(int month) {
+        mCal.set(Calendar.MONTH, month - 1);
+
+        for (int i = 0; i < mCal.getActualMaximum(Calendar.DAY_OF_MONTH); i++) {
+            dayList.add("" + (i + 1));
+        }
+
+
+
+    }
+    //https://heum-story.tistory.com/6 커스텀뷰 만드는중
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View v =inflater.inflate(R.layout.fragment_tracking_note, container, false);
+        mCalendar =v.findViewById(R.id.calendar);
+
+        createCalendar();
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tracking_note, container, false);
+        return v;
+    }
+
+    /**
+     * 그리드뷰 어댑터
+     *
+     */
+    private class GridAdapter extends BaseAdapter {
+
+        private final List<String> list;
+
+        private final LayoutInflater inflater;
+
+        /**
+         * 생성자
+         *
+         * @param context
+         * @param list
+         */
+        public GridAdapter(Context context, List<String> list) {
+            this.list = list;
+            this.inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+
+        @Override
+        public int getCount() {
+            return list.size();
+        }
+
+        @Override
+        public String getItem(int position) {
+            return list.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            ViewHolder holder = null;
+
+            if (convertView == null) {
+                convertView = inflater.inflate(R.layout.item_calendarview, parent, false);
+                holder = new ViewHolder();
+
+                holder.tvItemGridView = (TextView)convertView.findViewById(R.id.tv_item_gridview);
+
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder)convertView.getTag();
+            }
+            holder.tvItemGridView.setText("" + getItem(position));
+
+            //해당 날짜 텍스트 컬러,배경 변경
+            mCal = Calendar.getInstance();
+            //오늘 day 가져옴
+            Integer today = mCal.get(Calendar.DAY_OF_MONTH);
+            String sToday = String.valueOf(today);
+            if (sToday.equals(getItem(position))) { //오늘 day 텍스트 컬러 변경
+                holder.tvItemGridView.setTextColor(getResources().getColor(R.color.colorDogDogBlue));
+            }
+            return convertView;
+        }
+    }
+
+    private class ViewHolder {
+        TextView tvItemGridView;
     }
 }
