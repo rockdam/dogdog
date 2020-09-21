fd47734d60811d8378901e05aacf5b534724a61c
rockdam <choi37338@naver.com>
2020-09-22 02:05:04 +0900

    산책 시간 시작/일시정지시 도넛 퍼센트 반영 완료

--- app/src/main/java/com/makeus/dogdog/src/HomeDogDog/startWalking/startWalking.java
+++ app/src/main/java/com/makeus/dogdog/src/HomeDogDog/startWalking/startWalking.java
@@ -1,29 +1,23 @@
 package com.makeus.dogdog.src.HomeDogDog.startWalking;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.animation.ObjectAnimator;
 import android.os.Bundle;
-import android.os.CountDownTimer;
 import android.os.SystemClock;
 import android.view.View;
-import android.view.animation.DecelerateInterpolator;
 import android.widget.Chronometer;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
 import android.widget.SeekBar;
-import android.widget.TextView;
 
 import com.makeus.dogdog.R;
 import com.makeus.dogdog.src.BaseActivity;
 
-public class startWalking extends BaseActivity implements SeekBar.OnSeekBarChangeListener {
+public class startWalking extends BaseActivity  {
 
     SeekBar seekBar;
     DonutView mDonutView;
-    Chronometer mWalkingTime;
+    Chronometer mWalkingTime,mWalkingDistance;
     ImageView mStartWalking;
-    private boolean running;
+    private int mTimetickin =0;
+    private boolean mRunning;
     private long timeWhenStopped = 0;
 
     @Override
@@ -33,10 +27,8 @@ public class startWalking extends BaseActivity implements SeekBar.OnSeekBarChang
         setContentView(R.layout.activity_start_walking);
         mStartWalking =findViewById(R.id.start_walking);
         mWalkingTime = findViewById(R.id.walking_time_startwalking);
-
-        seekBar = findViewById(R.id.seekBar);
+        mWalkingDistance = findViewById(R.id.walking_distance_startwalking);
         mDonutView = findViewById(R.id.donut);
-        seekBar.setOnSeekBarChangeListener(this);
 
 
         mWalkingTime.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
@@ -48,6 +40,23 @@ public class startWalking extends BaseActivity implements SeekBar.OnSeekBarChang
                 int s = (int) (time - h * 3600000 - m * 60000) / 1000;
                 String t = (h < 10 ? "0" + h : h) + ":" + (m < 10 ? "0" + m : m) + ":" + (s < 10 ? "0" + s : s);
                 chronometer.setText(t);
+                if(time %18000 <1000 && time >=18000) //18초에 1초씩 증가 .. 18초 이상일 때 부터 1%증가
+                {
+                    mTimetickin++;
+                    mDonutView.setValue(mTimetickin);
+                }
+//                if(true)
+//                {
+//                    mTimetickin++;
+//                    mDonutView.setValue(mTimetickin);
+//                }
+            }
+        });
+        mWalkingDistance.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
+            @Override
+            public void onChronometerTick(Chronometer chronometer) {
+                long time = SystemClock.elapsedRealtime()- chronometer.getBase();
+                chronometer.setText(time+"");
             }
         });
 
@@ -59,18 +68,20 @@ public class startWalking extends BaseActivity implements SeekBar.OnSeekBarChang
             @Override
             public void onClick(View view) {
 
-                if(!running) {
+                if(!mRunning) { //시작
 
                     mWalkingTime.setBase(SystemClock.elapsedRealtime() + timeWhenStopped);
                     mWalkingTime.start();
+                    mWalkingDistance.setBase(SystemClock.elapsedRealtime());
+                    mWalkingDistance.start();
                     mStartWalking.setImageResource(R.drawable.pause_pause);
-                    running =true;
-                }else{
+                    mRunning =true;
+                }else{ //일시 중지
 
                     timeWhenStopped = mWalkingTime.getBase() - SystemClock.elapsedRealtime();
                     mWalkingTime.stop();
                     mStartWalking.setImageResource(R.drawable.start);
-                    running =false;
+                    mRunning =false;
 
                 }
             }
@@ -78,29 +89,5 @@ public class startWalking extends BaseActivity implements SeekBar.OnSeekBarChang
     }
 
 
-    public void startWalkingTime() {
-        if (!running) {
 
-            mWalkingTime.start();
-            running = true;
-        }
-
-
-    }
-
-    @Override
-    public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
-        mDonutView.setValue(progress);
-
-    }
-
-    @Override
-    public void onStartTrackingTouch(SeekBar seekBar) {
-
-    }
-
-    @Override
-    public void onStopTrackingTouch(SeekBar seekBar) {
-
-    }
 }
