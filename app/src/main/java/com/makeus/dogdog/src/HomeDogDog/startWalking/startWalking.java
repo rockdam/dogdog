2d211839cb7bf9c82454d5f52ca53669023f4667
rockdam <choi37338@naver.com>
2020-09-21 18:21:11 +0900

    산책 시간 측정 만드는 중

--- app/src/main/java/com/makeus/dogdog/src/HomeDogDog/startWalking/startWalking.java
+++ app/src/main/java/com/makeus/dogdog/src/HomeDogDog/startWalking/startWalking.java
@@ -4,9 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.animation.ObjectAnimator;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.SystemClock;
+import android.view.View;
 import android.view.animation.DecelerateInterpolator;
+import android.widget.Chronometer;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 import com.makeus.dogdog.R;
 import com.makeus.dogdog.src.BaseActivity;
@@ -15,21 +21,71 @@ public class startWalking extends BaseActivity implements SeekBar.OnSeekBarChang
 
     SeekBar seekBar;
     DonutView mDonutView;
+    Chronometer mWalkingTime;
+    ImageView mStartWalking;
+    private boolean running;
+    private long timeWhenStopped = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_start_walking);
-//        ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
-//        ObjectAnimator animation = ObjectAnimator.ofInt(progressBar, "progress", 0, 500); // see this max value coming back here, we animate towards that value
-//        animation.setDuration(10000); // in milliseconds
-//        animation.setInterpolator(new DecelerateInterpolator());
-//        animation.start();
+        mStartWalking =findViewById(R.id.start_walking);
+        mWalkingTime = findViewById(R.id.walking_time_startwalking);
 
-
-        seekBar=findViewById(R.id.seekBar);
-        mDonutView=findViewById(R.id.donut);
+        seekBar = findViewById(R.id.seekBar);
+        mDonutView = findViewById(R.id.donut);
         seekBar.setOnSeekBarChangeListener(this);
+
+
+        mWalkingTime.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
+            @Override
+            public void onChronometerTick(Chronometer chronometer) {
+                long time = SystemClock.elapsedRealtime() - chronometer.getBase();
+                int h = (int) (time / 3600000);
+                int m = (int) (time - h * 3600000) / 60000;
+                int s = (int) (time - h * 3600000 - m * 60000) / 1000;
+                String t = (h < 10 ? "0" + h : h) + ":" + (m < 10 ? "0" + m : m) + ":" + (s < 10 ? "0" + s : s);
+                chronometer.setText(t);
+            }
+        });
+
+        mWalkingTime.setBase(SystemClock.elapsedRealtime());
+        mWalkingTime.setText("00:00:00");
+
+
+        mStartWalking.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(!running) {
+
+                    mWalkingTime.setBase(SystemClock.elapsedRealtime() + timeWhenStopped);
+                    mWalkingTime.start();
+                    mStartWalking.setImageResource(R.drawable.pause_pause);
+                    running =true;
+                }else{
+
+                    timeWhenStopped = mWalkingTime.getBase() - SystemClock.elapsedRealtime();
+                    mWalkingTime.stop();
+                    mStartWalking.setImageResource(R.drawable.start);
+                    running =false;
+
+                }
+            }
+        });
+    }
+
+
+    public void startWalkingTime() {
+        if (!running) {
+
+            mWalkingTime.start();
+            running = true;
+        }
+
+
     }
 
     @Override
