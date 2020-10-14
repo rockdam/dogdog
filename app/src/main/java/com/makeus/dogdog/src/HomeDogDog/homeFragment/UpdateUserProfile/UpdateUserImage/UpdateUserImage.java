9283e22038d9f90efb7caa6eecb09b0827eaec30
rockdam <choi37338@naver.com>
2020-10-14 19:29:27 +0900

    정보수정 2 , 3 화면 작성

---
+++ app/src/main/java/com/makeus/dogdog/src/HomeDogDog/homeFragment/UpdateUserProfile/UpdateUserImage/UpdateUserImage.java
@@ -0,0 +1,16 @@
+package com.makeus.dogdog.src.HomeDogDog.homeFragment.UpdateUserProfile.UpdateUserImage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.makeus.dogdog.R;
+
+public class UpdateUserImage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_user_image);
+    }
+}
