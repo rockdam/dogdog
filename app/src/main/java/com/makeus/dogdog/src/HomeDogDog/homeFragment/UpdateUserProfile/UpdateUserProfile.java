b178e689b3981d7443301c3960876b2ebd9ce7ca
rockdam <choi37338@naver.com>
2020-10-14 13:34:23 +0900

    정보 수정 틀 완성

---
+++ app/src/main/java/com/makeus/dogdog/src/HomeDogDog/homeFragment/UpdateUserProfile/UpdateUserProfile.java
@@ -0,0 +1,16 @@
+package com.makeus.dogdog.src.HomeDogDog.homeFragment.UpdateUserProfile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.makeus.dogdog.R;
+
+public class UpdateUserProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_user_profile);
+    }
+}
