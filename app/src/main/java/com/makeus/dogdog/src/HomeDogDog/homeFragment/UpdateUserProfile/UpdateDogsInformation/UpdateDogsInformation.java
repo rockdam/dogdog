94227febef645332e6a46a265e4e8ae4c41a6a1b
rockdam <choi37338@naver.com>
2020-10-14 19:52:04 +0900

    내정보 ,개 정보 수정 추가

---
+++ app/src/main/java/com/makeus/dogdog/src/HomeDogDog/homeFragment/UpdateUserProfile/UpdateDogsInformation/UpdateDogsInformation.java
@@ -0,0 +1,16 @@
+package com.makeus.dogdog.src.HomeDogDog.homeFragment.UpdateUserProfile.UpdateDogsInformation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.makeus.dogdog.R;
+
+public class UpdateDogsInformation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_dogs_information);
+    }
+}
