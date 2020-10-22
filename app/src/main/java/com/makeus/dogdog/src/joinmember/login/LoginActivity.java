9f63ed43be9babd28ea08fb4de1f4b6484c342ed
rockdam <choi37338@naver.com>
2020-10-22 14:52:30 +0900

    로그인 서버만 연결하면 됌

--- app/src/main/java/com/makeus/dogdog/src/joinmember/login/LoginActivity.java
+++ app/src/main/java/com/makeus/dogdog/src/joinmember/login/LoginActivity.java
@@ -1,15 +1,23 @@
 package com.makeus.dogdog.src.joinmember.login;
 
 import android.content.Intent;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.core.content.ContextCompat;
 
 import com.makeus.dogdog.R;
 import com.makeus.dogdog.src.BaseActivity;
-import com.makeus.dogdog.src.HomeDogDog.HomeActivity;
-import com.makeus.dogdog.src.joinmember.login.interfaces.MoveHomeAcitivity;
 import com.makeus.dogdog.src.joinmember.step1.Step1Activity;
+import com.makeus.dogdog.src.joinmember.step2.DuplicateUserIdService;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -17,8 +25,8 @@ import java.util.regex.Pattern;
 public class LoginActivity extends BaseActivity implements View.OnClickListener{
 
 
-    TextView mJoinmemberButton;
-    AutoLoginService mAutoLoginService;
+    TextView mJoinmemberButton,mLogin;
+    EditText mInputEmail,mInputPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +36,11 @@ public class LoginActivity extends BaseActivity implements View.OnClickListener{
         mJoinmemberButton=findViewById(R.id.joinmember_acitivity_join);
 
         mJoinmemberButton.setOnClickListener(this);
+        mLogin=findViewById(R.id.login_activity_join);
+        mInputEmail =findViewById(R.id.id_login);
+        mInputPassword=findViewById(R.id.password_login);
+
+        mLogin.setOnClickListener(this);
 
 
     }
@@ -42,7 +55,11 @@ public class LoginActivity extends BaseActivity implements View.OnClickListener{
         return matcher.matches();
 
     }
-
+    //    https://stackoverflow.com/questions/12947620/email-address-validation-in-android-on-edittext
+//    이메일 체크는 이걸로하면됌
+    public static boolean isValidEmail(CharSequence target) {
+        return (!TextUtils.isEmpty(target) && Patterns.EMAIL_ADDRESS.matcher(target).matches());
+    }
 
     @Override
     public void onClick(View view) {
@@ -58,8 +75,33 @@ public class LoginActivity extends BaseActivity implements View.OnClickListener{
 
                 startActivity(intent);
                 finish();
-                //이건 좀 생각해보자 .;
                 break;
+            case R.id.login_activity_join:
+
+
+                if (!isValidEmail(mInputEmail.getText().toString())) {
+
+
+                    Toast.makeText(this, "이메일 형식이 맞지 않습니다. \n다시 입력해주세요 :)", Toast.LENGTH_SHORT).show();
+
+                }else if( !isValidPassword(mInputPassword.getText().toString()))
+                {
+
+
+                    Toast.makeText(this, "패스워드 형식이 맞지 않습니다. \n다시 입력해주세요 :)", Toast.LENGTH_SHORT).show();
+                }
+
+
+                else {
+
+//서비스
+
+                }
+
+
+                break;
+
+
 
 
         }
